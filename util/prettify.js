2e5e71c296eae686699b4083ef863b8dfcbd53a5
rueedlinger <md@rueedlinger.ch>
2014-02-04 21:45:27 +0100

    updated json export for flume

--- util/prettify.js
+++ util/prettify.js
@@ -21,31 +21,7 @@ function toXsvFormat(tweet, delim) {
 	return out  + '\n';
 }
 
-module.exports.toCsvFormat = function(tweet) {
-	return toXsvFormat(tweet, ';');
-}
-
-module.exports.toTsvFormat = function(tweet) {
-	return toXsvFormat(tweet, '\t');
-}
-
-module.exports.toString = function(tweet) {
-	var created =  new Date(Date.parse(tweet.created_at));
-	var updated = new Date();
-	var id = tweet.id_str;
-	var text = tweet.text.replace('\t',' ').replace('#', '');
-	var lang = tweet.lang;
-	var rcount = tweet.retweet_count;
-	var user = tweet.user;
-	var userId = user.id_str;
-	var userName = user.screen_name.replace('\t',' ');
-	var followers = user.followers_count;
-
-	var out = 'tweet: ' + id + ' from the ' + created.toJSON()  + ' by user ' + userName + ' (' + userId + ')';
-	return out + '\n';
-}
-
-module.exports.toJson = function(tweet) {
+function toJson(tweet) {
 	var created =  new Date(Date.parse(tweet.created_at));
 	var updated = new Date();
 	var id = tweet.id_str;
@@ -74,4 +50,65 @@ module.exports.toJson = function(tweet) {
 	};
 	
 	return JSON.stringify(out) + '\n';
+}
+
+module.exports.toCsvFormat = function(tweet) {
+	return toXsvFormat(tweet, ';');
+}
+
+module.exports.toTsvFormat = function(tweet) {
+	return toXsvFormat(tweet, '\t');
+}
+
+module.exports.toString = function(tweet) {
+	var created =  new Date(Date.parse(tweet.created_at));
+	var updated = new Date();
+	var id = tweet.id_str;
+	var text = tweet.text.replace('\t',' ').replace('#', '');
+	var lang = tweet.lang;
+	var rcount = tweet.retweet_count;
+	var user = tweet.user;
+	var userId = user.id_str;
+	var userName = user.screen_name.replace('\t',' ');
+	var followers = user.followers_count;
+
+	var out = 'tweet: ' + id + ' from the ' + created.toJSON()  + ' by user ' + userName + ' (' + userId + ')';
+	return out + '\n';
+}
+
+module.exports.toJson = function(tweet) {
+	return toJson(tweet);
+}
+
+module.exports.toFlumeEventJson = function(tweet) {
+	var created =  new Date(Date.parse(tweet.created_at));
+	var updated = new Date();
+	var id = tweet.id_str;
+	var text = tweet.text.replace('\t',' ').replace('#', '');
+	var lang = tweet.lang;
+	var rcount = tweet.retweet_count;
+	var user = tweet.user;
+	var userId = user.id_str;
+	var userName = user.screen_name.replace('\t',' ');
+	var followers = user.followers_count;
+
+	var body = {
+		id: id,
+		createdYear: created.getFullYear(),
+		createdMonth: (created.getMonth() + 1),
+		createdDay:  created.getDate(),		
+		userName: userName,
+		userId: userId,
+		text: text,
+		lang: lang,
+		rcount: rcount,
+		followers: followers
+	};
+	
+	var event = {
+		timestamp: updated.getTime(),
+		body: body
+	};
+	
+	return JSON.stringify(event);
 }
