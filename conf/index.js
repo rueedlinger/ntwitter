f88d9d844197744b96e462e14c97abd3f00e10be
rueedlinger <md@rueedlinger.ch>
2014-02-17 22:56:17 +0100

    updated

--- conf/index.js
+++ conf/index.js
@@ -1,16 +1,16 @@
 var config = {
 	// twitter credentials
-	consumer_key:         'REPLACE',
-	consumer_secret:      'REPLACE',
-	access_token:         'REPLACE',
-	access_token_secret:  'REPLACE',
+	consumer_key:         'xxx',
+	consumer_secret:      'xxx',
+	access_token:         'xxx',
+	access_token_secret:  'xxx',
 	
 	// track this keywords
-	track: ['superbowl', 'football'],
+	track: ['zeugenaufruf', 'verkehrsunfall', 'Verkehrsstau', 'personenschaden'],
 	
 	// follow this screen names
-	follow: ['SuperBowl', 'Vikings'],
-	
+	follow: ['TCSverkehrA1', 'TCSVerkehrA2', 'TCSVerkehrA3', 'KapoGR', 'StapoSG', 'StadtpolizeiZH', 'PoliceBern', 'POLIZEI_SCHWEIZ', 'railinfo_sbb'],	
+
 	// server and port where the collected tweets should be send to
 	host: 'localhost',
 	port: 8124
